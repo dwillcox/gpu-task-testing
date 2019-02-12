b3db697287f52232c28c79cc0cc1062365313364
Donald E. Willcox <dewillcox@lbl.gov>
2019-02-12 13:04:02 -0800

    update with latest changes to the managed vector.

--- managed_vector/manual_vector.h
+++ managed_vector/manual_vector.h
@@ -5,10 +5,11 @@
 #include "unified.h"
 
 template<class T> class GenericVector : public UnifiedMemoryClass {
+
+public:
     size_t filled_size;
     size_t allocated_size;
-    T* data_ptr;
-public:
+    T* data_ptr;    
     GenericVector(size_t create_size = 0) {
         cudaError_t cuda_status = cudaSuccess;
         cudaMallocManaged(&data_ptr, sizeof(T)*create_size);
@@ -52,17 +53,21 @@ public:
     void push_back(T element) {
         std::cout << "pushing back into vector" << std::endl;
         if (filled_size+1 > allocated_size) resize(max(2*allocated_size, static_cast<size_t>(1)));
+        //*(data_ptr + filled_size) = element;
         new (data_ptr + filled_size) T(element);
         filled_size++;
     }
     
     void resize(size_t new_size) {
-        std::cout << "resizing vector to " << new_size << std::endl;        
-        cudaError_t cuda_status = cudaFree(data_ptr);
-        assert(cuda_status == cudaSuccess);
-        cuda_status = cudaMallocManaged(&data_ptr, sizeof(T)*new_size);
-        assert(cuda_status == cudaSuccess);
-        allocated_size = new_size;        
+        std::cout << "resizing vector to " << new_size << std::endl;
+        if (new_size == 0) filled_size = 0;
+        else {
+            cudaError_t cuda_status = cudaFree(data_ptr);
+            assert(cuda_status == cudaSuccess);
+            cuda_status = cudaMallocManaged(&data_ptr, sizeof(T)*new_size);
+            assert(cuda_status == cudaSuccess);
+            allocated_size = new_size;
+        }
     }
 
     __host__ __device__ size_t size() {
