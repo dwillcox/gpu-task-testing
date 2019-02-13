a8e1342b4de78f441aa533794db05e1b806c909c
Donald E. Willcox <dewillcox@lbl.gov>
2019-02-12 21:17:08 -0800

    Fix bug in resize function for unified memory vector where the current vector contents were not retained.

--- features/unified_vector/unified_vector.h
+++ features/unified_vector/unified_vector.h
@@ -2,6 +2,8 @@
 #define DEVICE_VECTOR_H
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <cassert>
+#include <iostream>
 #include "unified.h"
 
 template<class T> class UnifiedVector : public UnifiedMemoryClass {
@@ -9,13 +11,15 @@ template<class T> class UnifiedVector : public UnifiedMemoryClass {
 public:
     size_t filled_size;
     size_t allocated_size;
-    T* data_ptr;    
+    T* data_ptr;
+
     UnifiedVector(size_t create_size = 0) {
         cudaError_t cuda_status = cudaSuccess;
         cudaMallocManaged(&data_ptr, sizeof(T)*create_size);
         cuda_status = cudaDeviceSynchronize();
-        assert(cuda_status == cudaSuccess);        
+        assert(cuda_status == cudaSuccess);
         allocated_size = create_size;
+        filled_size = 0;
     }
   
     ~UnifiedVector() {
@@ -51,21 +55,30 @@ public:
     }
 
     void push_back(T element) {
-        std::cout << "pushing back into vector" << std::endl;
+        std::cout << "pushing back into vector";
         if (filled_size+1 > allocated_size) resize(max(2*allocated_size, static_cast<size_t>(1)));
-        //*(data_ptr + filled_size) = element;
         new (data_ptr + filled_size) T(element);
         filled_size++;
+        std::cout << ", size is now " << this->size() << std::endl;
+        assert(element == (*this)[filled_size-1]);
     }
     
     void resize(size_t new_size) {
-        std::cout << "resizing vector to " << new_size << std::endl;
         if (new_size == 0) filled_size = 0;
         else {
-            cudaError_t cuda_status = cudaFree(data_ptr);
+            T* new_data_ptr;
+            cudaError_t cuda_status = cudaSuccess;
+
+            cuda_status = cudaMallocManaged(&new_data_ptr, sizeof(T)*new_size);
             assert(cuda_status == cudaSuccess);
-            cuda_status = cudaMallocManaged(&data_ptr, sizeof(T)*new_size);
+            
+            size_t num_elements_to_copy = min(new_size, filled_size);
+            memcpy(new_data_ptr, data_ptr, sizeof(T)*num_elements_to_copy);
+
+            cuda_status = cudaFree(data_ptr);
             assert(cuda_status == cudaSuccess);
+
+            data_ptr = new_data_ptr;
             allocated_size = new_size;
         }
     }
--- source/unified_vector.h
+++ source/unified_vector.h
@@ -11,19 +11,21 @@ template<class T> class UnifiedVector : public UnifiedMemoryClass {
 public:
     size_t filled_size;
     size_t allocated_size;
-    T* data_ptr;    
+    T* data_ptr;
+
     UnifiedVector(size_t create_size = 0) {
         cudaError_t cuda_status = cudaSuccess;
         cudaMallocManaged(&data_ptr, sizeof(T)*create_size);
         cuda_status = cudaDeviceSynchronize();
-        assert(cuda_status == cudaSuccess);        
+        assert(cuda_status == cudaSuccess);
         allocated_size = create_size;
+        filled_size = 0;
     }
-  
+
     ~UnifiedVector() {
         cudaError_t cuda_status = cudaFree(data_ptr);
         allocated_size = 0;
-        assert(cuda_status == cudaSuccess);        
+        assert(cuda_status == cudaSuccess);
     }
 
     __host__ __device__ T* begin() {
@@ -57,14 +59,23 @@ public:
         new (data_ptr + filled_size) T(element);
         filled_size++;
     }
-    
+
     void resize(size_t new_size) {
         if (new_size == 0) filled_size = 0;
         else {
-            cudaError_t cuda_status = cudaFree(data_ptr);
+            T* new_data_ptr;
+            cudaError_t cuda_status = cudaSuccess;
+
+            cuda_status = cudaMallocManaged(&new_data_ptr, sizeof(T)*new_size);
             assert(cuda_status == cudaSuccess);
-            cuda_status = cudaMallocManaged(&data_ptr, sizeof(T)*new_size);
+
+            size_t num_elements_to_copy = min(new_size, filled_size);
+            memcpy(new_data_ptr, data_ptr, sizeof(T)*num_elements_to_copy);
+
+            cuda_status = cudaFree(data_ptr);
             assert(cuda_status == cudaSuccess);
+
+            data_ptr = new_data_ptr;
             allocated_size = new_size;
         }
     }
